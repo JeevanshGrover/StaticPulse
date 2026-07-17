b7b59a0f09a531d66dd1db795489224c58a83be4
Jeevansh Grover <groverjeevansh0243@gmail.com>
2026-07-17 18:07:29 +0530

    initial setup

---
+++ Backend/src/index.js
@@ -0,0 +1,12 @@
+import { app } from "./app";
+import dotenv from 'dotenv'
+
+dotenv.config({
+    path: './.env'
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`)
+})
