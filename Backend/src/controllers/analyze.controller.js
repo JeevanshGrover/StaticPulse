c6eea8e68c165fd1b01a31ddd7229cfbc785a7f5
Jeevansh Grover <groverjeevansh0243@gmail.com>
2026-07-19 22:35:46 +0530

    integrate AI analysis in analyzeRepo function

--- Backend/src/controllers/analyze.controller.js
+++ Backend/src/controllers/analyze.controller.js
@@ -2,8 +2,9 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { validateRepo } from "../services/github/validateRepo.js";
 import { cloneRepo } from "../services/github/cloneRepo.js";
-import { scanRepo } from "../services/analysis/analyzers/fileScanner.js";
+import { runAnalyzers } from "../services/analysis/analyzerRunner.js"
 import { cleanupRepo } from "../utils/tempManager.js";
+import { getAIAnalysis } from "../services/ai/gemini.js"
 import { ApiError } from "../utils/ApiError.js";
 
 const analyzeRepo = asyncHandler(async(req, res) => {
@@ -15,19 +16,29 @@ const analyzeRepo = asyncHandler(async(req, res) => {
     let repoPath;
     try{
         validateRepo(repoUrl)
+        console.log(`repo validated`)
 
         const cloned = await cloneRepo(repoUrl)
+        console.log("repo cloned: clonedPath= ", cloned)
         repoPath = cloned.path;
+        console.log("repo cloned: ", repoPath)
 
-        const scanResult = await scanRepo(repoPath);
-
-        if(!scanResult){
-            throw new ApiError(500, "Internal server Error")
+        const metrics = await runAnalyzers(repoPath);
+        console.log("metrics: ", metrics)
+        if(!metrics){
+            throw new ApiError(500, "Internal server Error: Unable to fetch metrics")
+        }
+        
+        const aiAnalysis = await getAIAnalysis(metrics)
+        console.log("aiAnalysis result: ", aiAnalysis)
+        const report = {
+            metrics,
+            ...aiAnalysis,
         }
 
         return res
             .status(200)
-            .json(new ApiResponse(200, scanResult, "repository analyzed successfully"))
+            .json(new ApiResponse(200, report, "repository analyzed successfully"))
     } finally {
         if(repoPath){
             await cleanupRepo(repoPath)
